c68d42b52cffa45cf98018bc475bdec294bc5a83
Mingyu Lee <68541406+KoreaMGLEE@users.noreply.github.com>
2023-01-28 14:49:33 +0900

    Add files via upload

--- script/basicconcept_selection.py
+++ script/basicconcept_selection.py
@@ -1,24 +1,32 @@
 import os
 import re
 import pickle
-from src.module import split_text_ngram, count_concept_
+from src.module import count_concept_
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--conceptnet_path", default='../data/preprocessed_conceptnet', type=str)
+parser.add_argument("--topk_connected_concepts", default=5000, type=int)
+parser.add_argument("--corpus_dir", default='../data/pre-training-corpus/', type=str)
+parser.add_argument("--delete_threshold", default=100000, type=int)
+parser.add_argument("--basicConcepts_num", default=3000, type=int)
+parser.add_argument("--save_path", default='../data/concept_based_curriculum/basic_concepts', type=str)
+args = parser.parse_args()
 
-with open('../data/preprocessed_conceptnet', 'rb') as f:
+with open(args.conceptnet_path, 'rb') as f:
     conceptnet = pickle.load(f)
-
 
 ## select top k concepts based on the number of connected edges
 edge_count = {}
 for key in conceptnet.keys():
     edge_count[key] = len(conceptnet[key])
 
-topk = 5000
+topk = args.topk_connected_concepts
 topk_connected_concepts = {k.replace('_', ' '): v for k, v in sorted(edge_count.items(), key=lambda item: item[1])[-topk:]}
 
 
 ## count frequency in the corpus
-corpus_dir = '../data/pre-training-corpus/'
+corpus_dir = args.corpus_dir
 ConceptCount = {}
 for file in os.listdir(corpus_dir):
     with open(os.path.join(corpus_dir, file), 'r') as f:
@@ -27,17 +35,17 @@ for file in os.listdir(corpus_dir):
 
 
 ## delete concepts occurring less than 100k times
-delete_threshold = 100000
+delete_threshold = args.delete_threshold
 
 intermediate_conceptsSet = {}
 for key in ConceptCount.keys():
     if ConceptCount[key] > delete_threshold:
         intermediate_conceptsSet[key] = topk_connected_concepts[key]
 
-number_of_basicConcepts = 3000
+number_of_basicConcepts = args.basicConcepts_num
 basic_concepts = {k.replace('_', ' '): v for k, v in sorted(intermediate_conceptsSet.items(), key=lambda item: item[1])[-number_of_basicConcepts:]}
 
 
-with open('../data/concept_based_curriculum/basic_concepts', 'wb') as f:
+with open(args.save_path, 'wb') as f:
     pickle.dump(basic_concepts, f)
 
